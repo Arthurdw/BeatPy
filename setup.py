52fb67d52fa0d1c1e7953ec1018ae1e1b963ccef
Arthur De Witte <arthur.dewitte@gmail.com>
2020-02-08 14:28:24 +0100

    PyPi package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name='BeatPy',
+  packages=['BeatPy'],
+  version='v0.0.3',
+  license='MIT',
+  description='A python utility library to make your python coder life easier!',
+  author='Arthurdw (Arthur De Witte)',
+  author_email='mail.arthurdw@gmail.com',
+  url='https://github.com/Arthurdw/BeatPy',
+  download_url='https://github.com/Arthurdw/BeatPy/archive/v0.0.3.tar.gz',
+  keywords=['discord', 'util', 'beatpy'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
